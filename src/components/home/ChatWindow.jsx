dedaed04eb5fe61f816703a0e0e5e863dd979752
Mansi <sonawanemss21@gmail.com>
2025-03-30 03:22:15 -0500

    updated chatbox url

--- src/components/home/ChatWindow.jsx
+++ src/components/home/ChatWindow.jsx
@@ -31,7 +31,7 @@ export default function ChatWindow({ open, onClose }) {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/chat", {
+      const res = await fetch("https://bizfinder-zjjh.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userInput }),
